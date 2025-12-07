492bae4284fdda3aed0f8726fdffefaeded5c5bb
codex-pet <peterayono143@gmail.com>
2025-12-08 00:35:36 +0800

    Convert api entry to ESM and fix SPA routing

--- api/index.cjs
+++
@@ -1,4 +0,0 @@
-// Note the .cjs extension here
-const app = require('../backend/server.cjs');
-
-module.exports = app;
---
+++ api/index.js
@@ -0,0 +1,4 @@
+// api/index.js
+import app from '../backend/server.cjs';
+
+export default app;
